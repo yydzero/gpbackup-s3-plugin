cfadc59f4fbd5bdd4b22e8f9705b46aac4b6c175
Soumyadeep Chakraborty <sochakraborty@pivotal.io>
2018-07-06 13:08:19 -0700

    Added fast-fail checks for args to plugin commands
    Checking and failing fast if any plugin command is not executed with the
    correct no of arguments.
    
    Authored-by: Soumyadeep Chakraborty <sochakraborty@pivotal.io>

--- gpbackup_s3_plugin.go
+++ gpbackup_s3_plugin.go
@@ -5,6 +5,7 @@ package main
 import (
 	"log"
 	"os"
+	"fmt"
 
 	"github.com/greenplum-db/gpbackup-s3-plugin/s3plugin"
 	"github.com/urfave/cli"
@@ -23,38 +24,47 @@ func main() {
 		{
 			Name:   "setup_plugin_for_backup",
 			Action: s3plugin.SetupPluginForBackup,
+			Before: buildBeforeFunc(3),
 		},
 		{
 			Name:   "setup_plugin_for_restore",
 			Action: s3plugin.SetupPluginForRestore,
+			Before: buildBeforeFunc(3),
 		},
 		{
 			Name:   "cleanup_plugin_for_backup",
 			Action: s3plugin.CleanupPlugin,
+			Before: buildBeforeFunc(3),
 		},
 		{
 			Name:   "cleanup_plugin_for_restore",
 			Action: s3plugin.CleanupPlugin,
+			Before: buildBeforeFunc(3),
 		},
 		{
 			Name:   "backup_file",
 			Action: s3plugin.BackupFile,
+			Before: buildBeforeFunc(2),
 		},
 		{
 			Name:   "restore_file",
 			Action: s3plugin.RestoreFile,
+			Before: buildBeforeFunc(2),
 		},
 		{
 			Name:   "backup_data",
 			Action: s3plugin.BackupData,
+			Before: buildBeforeFunc(2),
 		},
 		{
 			Name:   "restore_data",
 			Action: s3plugin.RestoreData,
+			Before: buildBeforeFunc(2),
 		},
 		{
 			Name:   "plugin_api_version",
 			Action: s3plugin.GetAPIVersion,
+			Before: buildBeforeFunc(0),
 		},
 	}
 
@@ -62,4 +72,15 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func buildBeforeFunc(expectedNArg int)(beforeFunc cli.BeforeFunc) {
+	 beforeFunc = func(context *cli.Context) error {
+		if actualNArg := context.NArg(); actualNArg != expectedNArg {
+			return fmt.Errorf("Invalid number of arguments to plugin command. " +
+				"Expected %d arguments. Got %d arguments", expectedNArg, actualNArg)
+		}
+		return nil
+	}
+	return beforeFunc
 }
